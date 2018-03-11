809c4aa45067051f93dc2dbdd9bd681232a7eff3
Adam Hansen <adamlars90@gmail.com>
2018-03-11 17:48:15 +0100

    Added components with routing & added ng-bootstrap

---
+++ src/app/app.routing.ts
@@ -0,0 +1,22 @@
+import {RouterModule} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {ProductsComponent} from "./products/products.component";
+import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
+import {CheckOutComponent} from "./check-out/check-out.component";
+import {OrderSuccessComponent} from "./order-success/order-success.component";
+import {LoginComponent} from "./login/login.component";
+import {AdminProductsComponent} from "./admin/admin-products/admin-products.component";
+import {AdminOrdersComponent} from "./admin/admin-orders/admin-orders.component";
+
+export const AppRoutes = RouterModule.forRoot([
+  // Standard user routes
+  {path: '', component: HomeComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'shopping-cart', component: ShoppingCartComponent},
+  {path: 'check-out', component: CheckOutComponent},
+  {path: 'order-success', component: OrderSuccessComponent},
+  {path: 'login', component: LoginComponent},
+  // Admin routes
+  {path: 'admin/procucts', component: AdminProductsComponent},
+  {path: 'admin/orders', component: AdminOrdersComponent},
+]);
